33f11fdcf0ec537e9c2ece9a23497d6aa68667b5
Gabe-Zeigler <zeiglegl@mail.uc.edu>
2026-09-01 09:44:08 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    Scanner in = new Scanner(System.in);
+    String fName = "";
+    fName = SafeInput.getNonZeroLenString(in, "Enter your first name");
 
+    System.out.print("Hello" + ' ' + fName);
 }
